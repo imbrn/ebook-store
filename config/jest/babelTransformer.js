8620687f8e8872bc2ecc4d2248d7646389530422
Bruno C. Couto <brunodev02221@gmail.com>
2018-09-09 00:22:12 -0300

    Update to Babel 7

--- config/jest/babelTransformer.js
+++ config/jest/babelTransformer.js
@@ -1,6 +1,6 @@
 module.exports = require("babel-jest").createTransformer({
-  presets: ["env", "react"],
-  plugins: ["transform-class-properties"],
+  presets: ["@babel/env", "@babel/react"],
+  plugins: ["@babel/plugin-syntax-class-properties"],
   sourceMaps: "inline",
   retainLines: true
 });
